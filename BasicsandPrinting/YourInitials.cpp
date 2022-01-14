88542ab87b58d10127ff574e0ab8fdb5f5f81ff7
happykan <89062194+happykan@users.noreply.github.com>
2022-01-14 14:42:09 +0800

    c

---
+++ BasicsandPrinting/YourInitials.cpp
@@ -0,0 +1,16 @@
+//Basics and Printing Your Initials 6 pts
+#include <iostream>
+int main ()
+{
+    std::cout <<"For the name Jorge Balderama Perez...\n";
+    std::cout <<" \n";
+    std::cout <<"JJJJJ  BBBB   PPPP\n";
+    std::cout <<"  J    B   B  P   P\n";
+    std::cout <<"  J    B   B  P   P\n";
+    std::cout <<"  J    BBBB   PPPP \n";
+    std::cout <<"J J    B   B  P    \n";
+    std::cout <<"J J    B   B  P \n";
+    std::cout <<" JJ    BBBB   P\n";
+    return 0;
+    
+}
