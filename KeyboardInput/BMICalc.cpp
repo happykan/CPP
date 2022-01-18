2bdcd73ccb116e9051fdc5724e3ccaa1eb43574c
happykan <89062194+happykan@users.noreply.github.com>
2022-01-18 14:21:50 +0800

    c

--- KeyboardInput/BMICalc.cpp
+++ KeyboardInput/BMICalc.cpp
@@ -4,16 +4,27 @@ using namespace std;
 
 int main ()
 {
-    float height, weight, BMI;
-    std::cout <<"Your height in m: \n";
+    float height, weight, BMI, height_2;
+    //std::cout <<"Your height in inches: \n";
+    //cin >> height;
+
+    //std::cout <<"Your weight in pounds: \n";
+    //cin >> weight;
+
+    //BMI = (weight*0.453592)/(height*0.0254*height*0.0254);
+
+    //std::cout <<"Your BMI is " << BMI <<endl;
+    //return 0;
+    std::cout <<"Your height (feet only): \n";
     cin >> height;
 
-    std::cout <<"Your weight in kg: \n";
-    cin >> weight;
+    std::cout <<"Your weight (inches): \n";
+    cin >> height_2;
 
-    BMI = (weight)/(height*height);
+    std::cout <<"Your weight in pounds: \n";
+    cin >> weight;
+    BMI = (weight*0.453592)/((height_2*12*0.0254)+(height*0.0254)*(height_2*12*0.0254)+(height*0.0254));
 
     std::cout <<"Your BMI is " << BMI <<endl;
     return 0;
-
 }
