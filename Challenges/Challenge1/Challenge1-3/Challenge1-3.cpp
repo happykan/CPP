89e9b993e98648e99b53e7e8769a750c19c220a4
happykan <89062194+happykan@users.noreply.github.com>
2022-02-15 14:45:31 +0800

    c

---
+++ Challenges/Challenge1/Challenge1-3/Challenge1-3.cpp
@@ -0,0 +1,30 @@
+//Challenge #1-3
+#include <iostream>
+using namespace std;
+void myFunction (int a, int b, int n)
+{
+    int sum = 0;
+    for (int i=0; i<n; i++)
+    {
+        if (i % a==0 || i%b ==0)
+        {
+            sum = sum+i;
+        }
+        
+    }
+    cout <<sum;
+
+}
+int main ()
+{
+    int a, b, n;
+    cout <<"1st multiple: ";
+    cin >>a;
+    cout <<endl;
+    cout <<"2nd multiple: ";
+    cin >>b;
+    cout <<endl;
+    myFunction(a, b, 100);
+    cout <<endl;
+    return 0;
+}
