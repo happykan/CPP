a694e0e76fd48b338377c375acd81d24c09855e9
happykan <89062194+happykan@users.noreply.github.com>
2022-02-22 14:13:15 +0800

    c

---
+++ Challenges/Challenge3/Challenge3-4/Challenge3-4.cpp
@@ -0,0 +1,38 @@
+//Challenge3-4
+#include <iostream>
+#include <cmath>
+using namespace std;
+int sum;
+int findSum(int n)
+{
+    int t = 0;
+    for (int i=2; i<355000; i++)
+    {
+        sum=0;
+        int num=1;
+        int digit=i;
+        int temp=1;
+        while (digit>0)
+        {
+            temp=pow(digit%10,n);
+            digit/=10;
+            sum+=temp;
+        }
+        if(sum == i)
+        {
+            t+=i;
+            cout<<sum<<endl;
+        }
+        //else sum=0;
+    }
+    return t;
+}
+int main()
+{
+    int s;
+    cout<<"What power? ";
+    cin>>s;
+    int a= findSum(s);
+    cout<<"Sum = "<<a<<endl;;
+    return 0;
+}
